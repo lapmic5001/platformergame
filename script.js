6ddc37cab0a5f8751a7004446f835f364b77251a
lapmic5001 <michaelvlaposta@gmail.com>
2024-10-10 15:09:29 -0400

    1.0.0

--- script.js
+++ script.js
@@ -19,99 +19,108 @@ class MainScene extends Phaser.Scene {
     super({ key: 'MainScene' });
   }
 
-  preload() {
-    this.load.image('player', 'sprites/charactersprites/playerblock1.png');
-    this.load.image('platform', 'sprites/platformsprites/platformsprite.png');
-    this.load.image('background', 'sprites/platformsprites/bgsprite.png');
-    this.load.image('door', 'sprites/platformsprites/mysticaldoorway.png');
+  // In MainScene
+this.registry.set('playerData', { x: player.x, y: player.y });
+
+
+preload() {
+  this.load.image('player', 'sprites/charactersprites/playerblock1.png');
+  this.load.image('platform', 'sprites/platformsprites/platformsprite.png');
+  this.load.image('background', 'sprites/platformsprites/bgsprite.png');
+  this.load.image('door', 'sprites/platformsprites/mysticaldoorway.png');
+}
+
+create() {
+  this.add.image(400, 300, 'background');
+  platforms = this.physics.add.staticGroup();
+
+  // Create ground and platforms
+  platforms.create(400, 700, 'platform').setScale(12).refreshBody();
+  platforms.create(600, 400, 'platform');
+  platforms.create(50, 250, 'platform');
+  platforms.create(350, 250, 'platform');
+  platforms.create(750, 220, 'platform');
+
+  // Create player
+  player = this.physics.add.sprite(100, 450, 'player');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  // Enable collision between player and platforms
+  this.physics.add.collider(player, platforms, () => {
+    isJumping = false;
+    canDoubleJump = true;
+  });
+
+  // Create door sprite for room transition
+  const door = this.physics.add.staticImage(780, 450, 'door');
+  door.setSize(50, 100);
+  this.physics.add.overlap(player, door, this.enterRoom, null, this);
+
+  // Set up cursors for movement
+  cursors = this.input.keyboard.createCursorKeys();
+  this.input.keyboard.on('keydown-RIGHTCTRL', this.dash, this);
+}
+
+update() {
+  // Player movement left and right
+  if (cursors.left.isDown) {
+    player.setVelocityX(isDashing ? -320 : -160);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(isDashing ? 320 : 160);
+  } else {
+    player.setVelocityX(0);
   }
 
-  create() {
-    this.add.image(400, 300, 'background');
-    platforms = this.physics.add.staticGroup();
-
-    // Create ground and platforms
-    platforms.create(400, 700, 'platform').setScale(12).refreshBody();
-    platforms.create(600, 400, 'platform');
-    platforms.create(50, 250, 'platform');
-    platforms.create(350, 250, 'platform');
-    platforms.create(750, 220, 'platform');
-
-    // Create player
-    player = this.physics.add.sprite(100, 450, 'player');
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-    // Enable collision between player and platforms
-    this.physics.add.collider(player, platforms, () => {
-      isJumping = false;
+  // Jumping logic
+  if (cursors.up.isDown) {
+    if (!isJumping) {
+      player.setVelocityY(-330);
+      isJumping = true;
       canDoubleJump = true;
-    });
-
-    // Create door sprite for room transition
-    const door = this.physics.add.staticImage(780, 450, 'door');
-    door.setSize(50, 100);
-    this.physics.add.overlap(player, door, this.enterRoom, null, this);
-
-    // Set up cursors for movement
-    cursors = this.input.keyboard.createCursorKeys();
-    this.input.keyboard.on('keydown-RIGHTCTRL', this.dash, this);
-  }
-
-  update() {
-    // Player movement left and right
-    if (cursors.left.isDown) {
-      player.setVelocityX(isDashing ? -320 : -160);
-    } else if (cursors.right.isDown) {
-      player.setVelocityX(isDashing ? 320 : 160);
-    } else {
-      player.setVelocityX(0);
-    }
-
-    // Jumping logic
-    if (cursors.up.isDown) {
-      if (!isJumping) {
-        player.setVelocityY(-330);
-        isJumping = true;
-        canDoubleJump = true;
-      } else if (canDoubleJump) {
-        player.setVelocityY(-330);
-        canDoubleJump = false;
-      }
-    }
-
-    // Reset dash state
-    if (isDashing && this.time.now > dashTimer) {
-      isDashing = false;
-      player.setVelocityX(
-        cursors.left.isDown ? -160 : cursors.right.isDown ? 160 : 0
-      );
+    } else if (canDoubleJump) {
+      player.setVelocityY(-330);
+      canDoubleJump = false;
     }
   }
 
-  dash() {
-    if (!isDashing) {
-      isDashing = true;
-      dashTimer = this.time.now + 200; // Dash duration
-      player.setVelocityX(
-        cursors.right.isDown
-          ? 320
-          : cursors.left.isDown
+  // Reset dash state
+  if (isDashing && this.time.now > dashTimer) {
+    isDashing = false;
+    player.setVelocityX(
+      cursors.left.isDown ? -160 : cursors.right.isDown ? 160 : 0
+    );
+  }
+}
+
+dash() {
+  if (!isDashing) {
+    isDashing = true;
+    dashTimer = this.time.now + 200; // Dash duration
+    player.setVelocityX(
+      cursors.right.isDown
+        ? 320
+        : cursors.left.isDown
           ? -320
           : player.body.velocity.x
-      ); // Dash direction
-    }
+    ); // Dash direction
   }
+}
 
-  enterRoom() {
-    this.scene.start('NewRoomScene'); // Start the new room scene
-  }
+enterRoom() {
+  this.scene.start('NewRoomScene'); // Start the new room scene
+}
 }
 
 class NewRoomScene extends Phaser.Scene {
   constructor() {
     super({ key: 'NewRoomScene' });
   }
+
+  // In NewRoomScene
+  const playerData = this.registry.get('playerData');
+  player = this.physics.add.sprite(playerData.x, playerData.y, 'player');
+
 
   preload() {
     // Load assets for the new room here if necessary
@@ -122,16 +131,16 @@ class NewRoomScene extends Phaser.Scene {
   create() {
     this.add.image(400, 300, 'newBackground'); // New background
     const platforms = this.physics.add.staticGroup();
-    
+
     // Create new platforms
     platforms.create(400, 500, 'newPlatform').setScale(4).refreshBody();
     platforms.create(600, 350, 'newPlatform');
-    
+
     // You may want to add a new player instance or transition the existing player
     player = this.physics.add.sprite(100, 450, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
-    
+
     // Re-add the collider
     this.physics.add.collider(player, platforms);
 
